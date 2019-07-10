26c57aca1cbac5352deab4e209553c87aa5912d8
Manzilak <abdullah@manzilak.com>
2019-07-10 18:12:51 +0300

    2.0.0 React with Next

--- public/js/disqus.js
+++
@@ -1,6 +0,0 @@
-(function() {
-var d = document, s = d.createElement('script');
-s.src = 'https://codingride.disqus.com/embed.js';
-s.setAttribute('data-timestamp', +new Date());
-(d.head || d.body).appendChild(s);
-})();
--- settings.js
+++ settings.js
@@ -10,33 +10,7 @@ let settings = {
     xbAppID: 'bb42a7a45b0f183a8b3e8bca798b756b9ff140231bbe6c59df790fd4a9e95ea8:web',
     xbRecaptcha: '6LczQEgUAAAAAAzvdhR0Q37Q2lnVOdR49GW5MzI-',
     xbMaxRecords: 4
-  },
-  Website: {
-    title: 'codingRide',
-    description: 'Abdullah Alghamdi, a web developer resides in Jeddah, Saudi Arabia.',
-    url: 'https://codingride.com'
-  },
-  Menus: {
-    main_menu : {
-      home : '/',
-      about : '/about',
-      coding : '/category/coding',
-      blog : '/category/blog'
-    },
-    footer_menu : {
-        home : '/',
-        about : 'about',
-        contact : 'contact'
-    }
   }
 }
 
-settings.prepareConfigParams = (callback) => {
-  let params = {
-    config: settings.Website,
-    menus: settings.Menus
-  }
-  callback(false, params);
-}
-
-module.exports = settings;
+export default settings;
