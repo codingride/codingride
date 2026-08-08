e1814ad528dba04741f5fe00e6245c6256325747
Abdullah Alghamdi <abdullah@manzilak.com>
2026-08-08 20:54:00 +0300

    Add error handling for globalsData in kalimah.js

--- kalimah.js
+++ kalimah.js
@@ -79,6 +79,7 @@ app.get('/:type/:term/:offset?', (req, res) => {
   }
   
   settings.prepareConfigParams((globalsError, globalsData) => {
+    
     if(!globalsError && globalsData) {
       content.posts(type, term, offset, (postsError, postsData) => {
         if(!postsError && postsData) {
